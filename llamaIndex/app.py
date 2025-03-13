f69597646b41de6856e209166f46de44cc50ff4d
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2025-03-14 00:24:52 +0500

    load data

---
+++ llamaIndex/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
+
+openai.api_key = st.secrets.openai_key
+st.header("Chat with the Streamlit docs 💬 📚")
+
+if "messages" not in st.session_state.keys(): # Initialize the chat message history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+    ]
